9ca3e84e9289faf6150406a22a7c321f8e22a0e2
Petrified-Frog <petrified_frog@hotmail.com>
2021-06-18 11:38:35 +0200

    Add project files.

---
+++ TestingWithLists.Tests/PeopleTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestingWithLists;
+using Xunit;
+
+namespace TestingWithLists.Tests
+{
+    public class PeopleTests
+    {
+        [Fact]
+        public void AddPerson_ShouldIncreaseList()
+        {
+            Person newPerson = new Person { FirstName = "Bob", LastName = "Bobsson", Age = 15 };
+            
+            TestingWithLists.People.AddPerson(newPerson);
+            Assert.True(TestingWithLists.People.peopleList.Count == 1);
+            Assert.Contains<Person>(newPerson, TestingWithLists.People.peopleList);
+        }
+
+        [Theory]
+        [InlineData("Bob", "", 14, "LastName")]
+        [InlineData("  ", "Bobbson", 15, "FirstName")]
+        [InlineData("Bob", "Bobsson", -15, "Age")]
+        public void AddPerson_ShouldFailWhenPersonInvalid(string firstName, string lastName, int age, string badParam)
+        {
+            Person newPerson = new Person { FirstName = firstName, LastName = lastName, Age = age };
+            
+            Assert.Throws<ArgumentException>(badParam, () => TestingWithLists.People.AddPerson(newPerson));
+        }
+
+        [Fact]
+        public void SortPeopleByAge_ShouldSortPeopeList()
+        {
+            List<Person> people = new List<Person>();
+            people.Add(new Person { FirstName = "Bob", LastName = "Bobsson", Age = 15 });
+            people.Add(new Person { FirstName = "Ture", LastName = "Sventon", Age = 43 });
+            people.Add(new Person { FirstName = "Hans", LastName = "Gretson", Age = 8 });
+
+            List < Person > sortedList = new List<Person>();
+            sortedList = TestingWithLists.People.SortPeopleByAge(people);
+
+            Assert.Same(sortedList[0], people[2] );
+        }
+
+        [Fact]
+        public void SortPeopleByLastName_ShouldSortPeopeList()
+        {
+            List<Person> people = new List<Person>();
+            people.Add(new Person { FirstName = "Bob", LastName = "Bobsson", Age = 15 });
+            people.Add(new Person { FirstName = "Ture", LastName = "Sventon", Age = 43 });
+            people.Add(new Person { FirstName = "Hans", LastName = "Gretson", Age = 8 });
+
+            List<Person> sortedList = new List<Person>();
+            sortedList = TestingWithLists.People.SortPeopleByLastName(people);
+
+            Assert.Same(sortedList[0], people[0] );
+        }
+
+    }
+}
---
+++ TestingWithLists/People.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingWithLists
+{
+    public class Person
+    {
+        public string FirstName;
+        public string LastName;
+        public int Age;
+    }
+
+    public static class People
+    {
+        public static List<Person> peopleList = new List<Person>();
+
+        public static void AddPerson(Person newPerson)
+        {
+            //some validation
+            if (newPerson.Age < 0)
+                throw new ArgumentException("Age cannot be negative", "Age");
+            if (string.IsNullOrWhiteSpace(newPerson.FirstName))
+                throw new ArgumentException("Invalid first name", "FirstName");
+            if (string.IsNullOrWhiteSpace(newPerson.LastName))
+                throw new ArgumentException("Invalid last name", "LastName");
+
+            peopleList.Add(newPerson);
+        }
+
+        public static List<Person> SortPeopleByAge(List<Person> people)
+        {
+            List<Person> orderedList =  people.OrderBy(o => o.Age).ToList();
+            return orderedList;
+        }
+
+        public static List<Person> SortPeopleByLastName(List<Person> people)
+        {
+            List<Person> orderedList = people.OrderBy(o => o.LastName).ToList();
+            return orderedList;
+        }
+    }
+}
